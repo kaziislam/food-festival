848d0b86cff051eec86bcf19839155445b503fd7
Kazi Islam <kazichaska@gmail.com>
2023-02-05 12:17:40 -0600

    app is pwa now

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 module.exports = {
     entry: {
@@ -13,6 +13,13 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: __dirname + '/dist',
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './'),
+        },
+        compress: true,
+        port: 8080,
     },
     module: {
         rules: [
@@ -47,7 +54,22 @@ module.exports = {
             // We can also set this value to disable to temporarily stop the reporting and 
             // automatic opening of this report in the browser.
             analyzerMode: "static", //the report outputs to an HTML file in the dist folder
-        })
+        }),
+        new WebpackPwaManifest({
+            name: "Food Event",
+            short_name: "Foodies",
+            description: "An app that allows you to view upcoming food events.",
+            start_url: "../index.html",
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            fingerprints: false,
+            inject: false,
+            icons: [{
+              src: path.resolve("assets/img/icons/icon-512x512.png"),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join("assets", "icons")
+            }]
+          })
     ],
     mode: 'development'
 };
